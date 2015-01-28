7304acb21e2aaefbb980b8fad99bb51b63481c30
JasonY1 <jasonh.yi@gmail.com>
2015-01-28 13:11:32 -0800

    Profile/new view works

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,8 +12,7 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/new
   def new
-    @user = current_user
-    @profile = @user.build_profile(profile_params)
+    @profile = current_user.build_profile
   end
 
   # GET /profiles/1/edit
@@ -24,8 +23,7 @@ class ProfilesController < ApplicationController
   # POST /profiles
   # POST /profiles.json
   def create
-    @user = current_user
-    @profile = @user.build_profile(profile_params)
+    @profile = current_user.build_profile(profile_params)
 
     respond_to do |format|
       if @profile.save
