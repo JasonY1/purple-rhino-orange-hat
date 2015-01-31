3dac72c0a5a8289c81fa834a3453bde84cafc98a
JasonY1 <jasonh.yi@gmail.com>
2015-01-31 14:01:00 -0800

    Breaking changes: Paperclip attr accessor.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  attr_accessor :valid_id_file_name
   
   validates_presence_of :firstname, :lastname
   
@@ -8,16 +9,14 @@ class Profile < ActiveRecord::Base
   
   has_attached_file :prescription_id,
     styles: { normal: "400x400#", bigger: "700x700#" },
-    url: '/photos/profiles/prescription/:id/:style_:basename.:extension',
     default_style: :normal,
     preserve_files: true,
     processors: [:thumbnail, :compression]
 
-  validates_attachment :prescription_id, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment :prescription_id, content_type: { content_type: /^image\/(jpeg|png|gif|tiff|jpg)$/ }
   
   has_attached_file :valid_id,
     styles: { normal: "400x400#", bigger: "700x700#" },
-    url: '/photos/profiles/valid/:id/:style_:basename.:extension',
     default_style: :normal,
     preserve_files: true,
     processors: [:thumbnail, :compression]
